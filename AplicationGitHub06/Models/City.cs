222414e4df45a6c181de77c50487acaa207a8499
aplicativoGiht <wicente.paula@gmail.com>
2023-03-19 14:42:34 -0300

    PrimeiroComite

---
+++ AplicationGitHub06/Models/City.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AplicationGitHub06.Models
+{
+    public class City
+    {
+        public int MyProperty1 { get; set; }
+        public int MyProperty2 { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
