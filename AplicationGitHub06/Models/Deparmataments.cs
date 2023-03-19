1eb92fd998684e3011cffdc004c9a5235d6438d5
aplicativoGiht <wicente.paula@gmail.com>
2023-03-19 14:48:47 -0300

    departaments

---
+++ AplicationGitHub06/Models/Deparmataments.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AplicationGitHub06.Models
+{
+    public class Deparmataments
+    {
+        public int MyProperty10 { get; set; }
+    }
+}
