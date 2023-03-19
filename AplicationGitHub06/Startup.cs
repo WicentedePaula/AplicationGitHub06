395abb3617c1861e005e7b0073fcfa44b4273c28
aplicativoGiht <wicente.paula@gmail.com>
2023-03-19 14:35:23 -0300

    Adicionar arquivos de projeto.

---
+++ AplicationGitHub06/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AplicationGitHub06.Startup))]
+namespace AplicationGitHub06
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
